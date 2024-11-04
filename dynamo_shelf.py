9e5fb4f72e517e4df77ea3eedc835a2a14f7cbfb
cadizsd <143456301+cadizsd@users.noreply.github.com>
2024-11-04 09:04:53 -0500

    Update dynamo_shelf.py

--- dynamo_shelf.py
+++ dynamo_shelf.py
@@ -5,15 +5,14 @@ class DynamoDBShelf:
         dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
         self.table = dynamodb.Table('bookshelf')  # Ensure this matches your table name
 
-    def save_book(self, book_id, title, author, page_count, isbn):
+    def save_book(self, title, author, page_count, isbn):
         try:
             self.table.put_item(
                 Item={
-                    'BookID': book_id,  # Use the unique identifier for the partition key
+                    'BookID': isbn,  # Use the unique identifier for the partition key
                     'Title': title,
                     'Author': author,
                     'PageCount': page_count,
-                    'ISBN': isbn
                 }
             )
             return {'message': 'Book saved successfully!'}
