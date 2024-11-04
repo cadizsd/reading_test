2e60746f57aa2ce89a8f7a8bf2b32b39b3cbe1af
cadizsd <143456301+cadizsd@users.noreply.github.com>
2024-11-04 00:46:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,15 @@ def save_book():
     result = shelf.save_book(book_id, title, author, page_count, isbn)
     return jsonify(result)
 
+@app.route('/saved_books', methods=['GET'])
+def saved_books():
+    try:
+        response = shelf.table.scan()  # Scan the bookshelf table
+        return jsonify(response['Items'])  # Return the items in JSON format
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
 
